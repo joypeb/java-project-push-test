1af58c29b9e50265e08e209800bd08b12ecd8bf5
박은빈 <joypeb@naver.com>
2022-10-04 13:37:50 +0900

    돈세는 알고리즘

---
+++ src/MoneyTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MoneyTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean tf = true;
+        int[] moneyArr = {50000,10000,5000,1000};
+        int[] result = new int[4];
+
+        for(int i=0; i<moneyArr.length; i++) {
+            result[i] = num / moneyArr[i];
+            num -= moneyArr[i]*result[i];
+            System.out.println(moneyArr[i] + "는 " + result[i]);
+        }
+
+    }
+}
