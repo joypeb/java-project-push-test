5fdbf99728dff4f4b2e65673c5b25f74210cec35
박은빈 <joypeb@naver.com>
2022-10-04 16:17:18 +0900

    패키지 생성

---
+++ src/CodeUp/CodeUp_1001.java
@@ -0,0 +1,7 @@
+package CodeUp;
+
+public class CodeUp_1001 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/CodeUp/CodeUp_1004.java
@@ -0,0 +1,7 @@
+package CodeUp;
+
+public class CodeUp_1004 {
+    public static void main(String[] args) {
+        System.out.println("'Hello'");
+    }
+}
