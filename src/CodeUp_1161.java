00068cc19167c3e1853f17e5d823f3d0d7dd6c7e
박은빈 <joypeb@naver.com>
2022-10-04 15:34:50 +0900

    코드업 1161 짝수 홀수문제

--- src/CodeUp_1161.java
+++ src/CodeUp_1161.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
 public class CodeUp_1161 {
+    public static String getEvenOdd(int n) {
+        return n%2 == 0 ? "짝수" : "홀수";
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        String numStr = num1%2 == 0 ? "짝수" : "홀수";
-        String numStr2 = num2%2 == 0 ? "짝수" : "홀수";
-        String result = (num1+num2)%2 == 0 ? "짝수" : "홀수";
-        System.out.printf("%s+%s = %s\n", numStr, numStr2, result);
+        System.out.printf("%s+%s = %s\n", getEvenOdd(num1), getEvenOdd(num2), getEvenOdd(num1+num2));
     }
 }
