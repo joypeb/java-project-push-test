6b7e1d799913b3db16894c5474605274c7467f4f
박은빈 <joypeb@naver.com>
2022-10-06 16:03:48 +0900

    한줄읽기, 여러줄 읽기

---
+++ src/Java_221006/fileread/RealLineFile.java
@@ -0,0 +1,35 @@
+package Java_221006.fileread;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+
+class LineFile {
+    private String fileName;
+
+    public LineFile(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void readLine(String fileName) {
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String str;
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+        }catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
+
+public class RealLineFile {
+    public static void main(String[] args) {
+        LineFile lineFile = new LineFile("./src/Java_221006/fileread/a_file.txt");
+        lineFile.readLine(lineFile.getFileName());
+    }
+}
