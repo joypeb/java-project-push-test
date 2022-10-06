67de1d1b1e2b7dcec37f696432ecaaa1d4f03273
박은빈 <joypeb@naver.com>
2022-10-06 11:27:46 +0900

    Map 실습

---
+++ src/Java_221006/collection/MapExercise.java
@@ -0,0 +1,105 @@
+package Java_221006.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExercise {
+    public static void main(String[] args) {
+
+
+        Map<String, String> students = new HashMap<>();
+
+        students.put("권하준", "	https://github.com/dongyeon-0822/java-project-exercise");
+        students.put("조성윤", "	https://github.com/kang-subin/Java");
+        students.put("안예은", "	https://github.com/KoKwanwun/LikeLion.git");
+        students.put("남우빈", "	https://github.com/lcomment/Algorithm_Solution--Java/tree/main/LikeLion");
+        students.put("최경민", "	https://github.com/cmkxak/likelion-java-course");
+        students.put("안준휘", "	https://github.com/rnrudejr9/CodeLion-git-test");
+        students.put("하채민", "	https://github.com/Qkite/java-excercise-01/tree/main/src");
+        students.put("허도한", "	https://github.com/lucyoz/java-exercise");
+        students.put("배태훈", "	https://github.com/percyfrank/like-lion");
+        students.put("정해성", "	https://github.com/KimGunWoo9595/Lion_Java_Pratice");
+        students.put("하찬희", "	https://github.com/KimKiheon/LikeLion");
+        students.put("박도아", "	https://github.com/kny3037/java-exercise");
+        students.put("유경준", "	https://github.com/KimDohyoun79/remote_Lion.git");
+        students.put("고다니엘", "https://github.com/kimkimj/create-repo1");
+        students.put("하이안", "	https://github.com/mingry2/intellij-git-exercise1");
+        students.put("고유호", "	https://github.com/Min-ji99/intelliJ-git-exercies");
+        students.put("전민찬", "	https://github.com/sangho527/Like_Lion_Java");
+        students.put("배강", "https://github.com/ReadnThink/Like_lion.git");
+        students.put("유서이", "	https://github.com/yejinstar/git_exercise_push");
+        students.put("황도빈", "	https://github.com/dmdwns2/java-git-init");
+        students.put("강규진", "	https://github.com/Geuun/Java_Algorithm");
+        students.put("임예린", "	https://github.com/O-sulloc/java-exercise-1005");
+        students.put("양예온", "	https://github.com/chordpli/likelion_course");
+        students.put("곽윤재", "	https://github.com/strongcookdas/java.git");
+        students.put("한단", "https://github.com/Kim-Ji-Yeong/Java-exercise");
+        students.put("박강현", "	https://github.com/k0605ja/git-test");
+        students.put("윤성민", "	https://github.com/pigeon2gugu/like-lion-2/tree/main/src/class_221005");
+        students.put("박재윤", "	https://github.com/hj-k66/git_exercise_2");
+        students.put("서현준", "	https://github.com/moomineee/JavaEx");
+        students.put("노유주", "	https://github.com/sujin-park0607/java-project-exercise");
+        students.put("전율", "https://github.com/joypeb/java-project-push-test");
+        students.put("노아린", "	https://github.com/pi22-a/git_practice.git");
+        students.put("권아윤", "	https://github.com/JemJem70/Algolism/tree/master/src");
+        students.put("송태서", "	https://github.com/BearCrying/Git_Excercise/tree/main/src");
+        students.put("정라온", "	https://github.com/Bae-Ji-Won/Java-Study");
+        students.put("서서한", "	https://github.com/geunnseung/java_exercise_intellij_push.git");
+        students.put("문주호", "	https://github.com/JiMandy00/Java_LikeLion");
+        students.put("오세아", "	JAVA-GIT-TEST/src at main · seop93/JAVA-GIT-TEST (github.com)");
+        students.put("이호영", "	https://github.com/sjhlko/git-exercise-java");
+        students.put("손윤아", "	https://github.com/tgseo76/javja-ex");
+        students.put("이동우", "	https://github.com/seanee3670/CodeLion/tree/master/src");
+        students.put("남혜준", "	https://github.com/dalddu/Intellig-git-exercise");
+        students.put("황솔", "https://github.com/hyunsik96/git-exercise-java-push");
+        students.put("권은재", "	https://github.com/ccodding/git-exercise-java-push");
+        students.put("곽준휘", "	https://github.com/TaylorSwiftlove/git-exercise-java");
+        students.put("양시현", "	https://github.com/jinwoooooo/git-exercise-java-push");
+        students.put("김지훈", "	https://github.com/simhyunbo/git-exercise-04.git");
+        students.put("신서아", "	https://github.com/ahngiwon00/git-exercise-java-push");
+        students.put("노도형", "	https://github.com/ahn-ji0/java_exercise");
+        students.put("허선", "https://github.com/Changbum97/like-lion-java");
+        students.put("노지아", "	https://github.com/SoonMinEom/LikeALion");
+        students.put("신아인", "	https://github.com/ohy1023/newtest");
+        students.put("양시은", "	https://github.com/SuInWoo/LikeLion2th");
+        students.put("노석현", "");
+        students.put("손강준", "	https://github.com/inkyu-yoon/IntelliJ_Commit_Test");
+        students.put("백주안", "	https://github.com/KaNoopy/like_lion.git");
+        students.put("강단우", "	https://github.com/ondayary/java-init-project.git");
+        students.put("오나은", "	https://github.com/dohyunre702/codelion_intelliJ");
+        students.put("오한겸", "	https://github.com/leejoonsang/bes2");
+        students.put("백예준", "	https://github.com/naegonggae/lion");
+        students.put("노은우", "	https://github.com/Soyeong4250/IntelliJPush");
+        students.put("손도헌", "	https://github.com/slrkajsep337/JavaStudy");
+        students.put("전진우", "	https://github.com/ajin0516/java_project.git");
+        students.put("안주하", "	https://github.com/yjyj1023/java-Calculator2");
+        students.put("유민기", "	https://github.com/hyeonju01/likelion_repo3_test/tree/main/src/week3/day2");
+        students.put("노휘", "https://github.com/hakjun1/intelij-git-exercise03");
+        students.put("박채원", "	https://github.com/jangseoyun/Intellij-git-exercise");
+        students.put("강다겸", "	https://github.com/Senna97/Like-Lion");
+        students.put("신여준", "	https://github.com/zinxeon/git-exercise-push");
+        students.put("허은유", "	https://github.com/su0545/like-lion-java.git");
+        students.put("심서후", "	https://github.com/Nickolodeon98/second-java-exercise");
+        students.put("유준이", "	https://github.com/daehwan0307/JAVA_Study");
+        students.put("전민승", "	https://github.com/sanghee2359?tab=repositories");
+        students.put("강예은", "	https://github.com/wogus0518/LikeLion-Backend");
+        students.put("허로이", "	https://github.com/heejunns/git_exercise_push.git");
+        students.put("유유진", "	https://github.com/h5000n/git-java-ex211005");
+        students.put("권건하", "	https://github.com/mooonju/java-exercise");
+        students.put("하도이", "	https://github.com/coastby/java-project");
+        students.put("노도원", "	https://github.com/Yunjaejo/new-project-push-test");
+        students.put("문하성", "	https://github.com/Combi153/BackEndSchool2");
+        students.put("오시후", "	https://github.com/chlalswns200/java_push");
+        students.put("이로윤", "	https://github.com/S2uJeong/git-exercise-java");
+        students.put("노지후", "	https://github.com/gemdoq/exercise");
+        students.put("권채이", "	https://github.com/surpmh/intellij-git-exercise");
+        students.put("전담", "https://github.com/sandee-han/java-project-push-exercise");
+        students.put("하태율", "	https://github.com/JinDDung2/java-project/tree/main");
+        students.put("노다율", "	https://github.com/menuhwang/LikeLion");
+        students.put("장우솔", "	https://github.com/cvivis/git_practice2022/tree/main");
+
+        students.put("황하준", "	https://github.com/lay423/java-project");
+
+        System.out.println(students.get("황하준"));
+    }
+}
