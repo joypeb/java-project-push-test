2e7260aacf51ee6285da6deaa1618407c3fd176f
박은빈 <joypeb@naver.com>
2022-10-14 09:45:50 +0900

    삽입정렬 메소드 추가, swap

--- src/algorithm/InsertionSort.java
+++ src/algorithm/InsertionSort.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class InsertionSort {
     private int[] solution(int[] arr) {
-
-        for(int i=1; i<arr.length; i++) {
-            for(int j=i-1; j>i; j--) {
-
-            }
+        int i = 1;
+        int temp = 0;
+        if(arr[i] < arr[i-1]) {
+            temp = arr[i-1];
+            arr[i-1] = arr[i];
+            arr[i] = temp;
         }
-
 
         return arr;
     }
@@ -18,7 +18,7 @@ public class InsertionSort {
     public static void main(String[] args) {
         InsertionSort insertionSort = new InsertionSort();
 
-        int[] arr = {3,44,2,63,8,1};
+        int[] arr = {333,44,2,63,8,1};
 
         int[] result = insertionSort.solution(arr);
 
