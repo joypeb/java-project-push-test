ad29e7edbabc6b48f48405724fbe8944a298b782
박은빈 <joypeb@naver.com>
2022-10-17 09:22:02 +0900

    별찍기 4단

--- src/algorithm/RightTriangle.java
+++ src/algorithm/RightTriangle.java
@@ -1,10 +1,18 @@
 package algorithm;
 
 public class RightTriangle {
+    public void rightTriangle(int num) {
+        for(int i=0; i<num; i++) {
+            for(int j=0; j<i+1; j++) {
+                System.out.printf("*");
+            }
+            System.out.printf("\n");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        RightTriangle rightTriangle = new RightTriangle();
+
+        rightTriangle.rightTriangle(4);
     }
 }
